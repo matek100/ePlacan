c8dae450e5d385d3887925a5255fc28e8e90a53f
daboz5 <aljaz.bozicko@aol.com>
2023-11-15 01:39:44 +0100

    modified:   src/app.css 	modified:   src/components/PayForm.tsx 	modified:   src/routes/eplacan.css

--- src/components/PayForm.tsx
+++ src/components/PayForm.tsx
@@ -1,26 +1,61 @@
+import { useEffect } from "preact/hooks"
+
 export default function PayForm() {
+
+    useEffect(() => {
+        createSelection();
+    }, [])
+
+
+    /* WORK IN PROGRESS, POSKUŠAM IZBOLJŠATI SELECTION, VENDAR JE POTREBNO VSE NA ROKE, KER CSS NE DELUJE */
+    const createSelection = () => {
+        const sel = document.getElementsByTagName("select")[0];
+        const opts = sel.options;
+        const optsl = opts.length;
+
+        const newOpts = document.createElement("div");
+        newOpts.setAttribute("class", "options");
+        newOpts.innerHTML = sel.options[sel.selectedIndex].innerHTML;
+
+        const newOpt = document.createElement("div");
+        newOpt.setAttribute("class", "option");
+
+        document.getElementById("select-box")?.append(newOpts);
+
+        for (let i = 0; i < optsl; i++) {
+            newOpts.append(newOpt);
+        }
+    }
+
     return (
-        <form id="form">
-            <label class={"form-input"}>
-                <span>Naziv poklica:</span>
-                <input type={"text"}></input>
+        <form id="form colFlex">
+
+            <label class={"form-input flex"}>
+                <span>Naziv poklica :</span>
+                <input name="job-input" type={"text"} class={"form-input-field"} required></input>
             </label>
-            <label class={"form-input"}>
-                <span>Povprečje ur na teden:</span>
-                <input type={"number"}></input>
+
+            <label class={"form-input flex"}>
+                <span>Povprečje ur na teden :</span>
+                <input name="time-input" type={"number"} class={"form-input-field"} required></input>
             </label>
-            <label class={"form-input"}>
-                <span>Mesečni prihodek:</span>
-                <input type={"number"}></input>
+
+            <label class={"form-input flex"}>
+                <span>Mesečni prihodek :</span>
+                <input name="pay-input" type={"number"} class={"form-input-field"} required></input>
             </label>
-            <label class={"form-input"}>
-                <span>Dosežena izobrazba:</span>
-                <select>
-                    <option></option>
-                    <option>Option One</option>
-                    <option>Option Two</option>
-                </select>
+
+            <label class={"form-input flex"}>
+                <span>Dosežena izobrazba :</span>
+                <div id="select-box">
+                    <select name="school-input" class={"form-input-field"} required>
+                        <option value="">-- Klikni in izberi --</option>
+                        <option value="option-one">Option One</option>
+                        <option value="option-two">Option Two</option>
+                    </select>
+                </div>
             </label>
+
         </form>
     )
 }
