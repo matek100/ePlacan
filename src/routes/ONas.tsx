baae80dd21bd03bcab404dc33d95e9c6a719c68a
daboz5 <aljaz.bozicko@aol.com>
2023-12-15 04:14:30 +0100

    modified:   src/app.css modified:   src/index.css 	modified:   src/routes/ONas.tsx

--- src/routes/ONas.tsx
+++ src/routes/ONas.tsx
@@ -17,9 +17,9 @@ export default function ONas() {
         <section id="explainBox" class={"defMouse"}>
             <p>ePlačan je namenjen spodbujanju deljenja informacij o prejetem plačilu za opravljeno delo, še posebno med mladimi.</p>
             <p>Informirani smo sposobnejši oceniti stanje na trgu dela, recimo če smo za delo ustrezno plačani.</p>
-            <p>Boljša informiranost spodbuja konkurenčno vedenje in je posledično v korist tako delavcev kot delodajalcev.</p>
+            <p>Boljša informiranost spodbuja konkurenčno vedenje in je v korist tako delavcev kot delodajalcev.</p>
             <p>Delavcev, ker dobijo nujne informacije o oceni vrednosti svojega dela na trgu dela.</p>
-            <p>Delodajalcev, ker s ponudbo nadstandardnih pogojev lažje prepričajo delavce o vrednost svoje ponudbe.</p>
+            <p>Delodajalcev, ker s ponudbo nadstandardnih pogojev lažje prepričajo delavce v vrednost svoje ponudbe.</p>
             <p>Spodbujamo delavce in delodajalce, spodbujamo konkurenco, spodbujamo dobre delovne prakse.</p>
         </section>
         <section id="promoBox" class={"flex"}>
